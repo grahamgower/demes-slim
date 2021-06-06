b3f31e0dc0cae0495a10e216fbdbbc25d270219a
Graham Gower <graham.gower@gmail.com>
2021-06-06 13:59:38 +0200

    Use ALL_CAPS for constants.

--- tests/tests.sh
+++ tests/tests.sh
@@ -11,14 +11,19 @@ done
 
 for Q in 1 10.0; do
     for json in ../examples/*.json; do
-        echo "Q=$Q; $json"
+        echo "SCALING_FACTOR=$Q; JSON_FILE=$json"
         states_file=`mktemp`
-        slim -d Q=$Q -d json=\"$json\" test_demes_schedule_events.slim \
+        slim \
+            -d SCALING_FACTOR=$Q \
+            -d JSON_FILE=\"$json\" \
+            test_demes_schedule_events.slim \
             > $states_file \
             || die "test_demes_schedule_events.slim: failed for $json"
+
         yaml=${json%%.json}.yaml
         ./verify_slim_against_demes.py -Q $Q $states_file $yaml \
             || die "verify_slim_against_demes.py: failed for $yaml"
+
         rm $states_file
     done
 done
